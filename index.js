6930ca7e599fd0bbd929223d2b00ef953d4f03ed
Vlad <aguretsvlad@gmail.com>
2020-01-04 19:33:53 +0200

    pr
    pr2
    
    pr3
    
    pr4
    
    pr5
    
    pr6
    
    pr7
    
    pr
    
    
    Fixes
    
    
    fix

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hi');
+function initCornerRoundDivs () {
+  const sections = document.querySelectorAll('section')
+  const divAligns = ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+  sections.forEach(section => {
+    for (const align of divAligns) {
+      section.appendChild(createCornerDiv(align))
+    }
+  })
+}
+
+function createCornerDiv (align) {
+  const el = document.createElement('div')
+  el.classList.add('corner-div')
+  el.classList.add(align)
+  return el
+}
+
+function onSwitchChange () {
+  document.body.classList.toggle('-dark');
+}
+
+initCornerRoundDivs()
