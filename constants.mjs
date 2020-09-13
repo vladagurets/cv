65de29d8a75f33961ab2dcca0de0a9ddd60a63be
vladagurets <aguretsvlad@gmail.com>
2020-09-13 14:05:22 +0300

    Updates

---
+++ constants.mjs
@@ -0,0 +1,25 @@
+export const TECH_SKILLS = [
+  'Docker',
+  'JavaScript',
+  'MongoDB',
+  'NodeJS',
+  'ReactJS',
+  'Unix'
+]
+
+export const HARD_SKILLS = [
+  'Data mining/migration',
+  'System migration',
+  'System/module designing',
+  'Web development'
+]
+
+export const SOFT_SKILLS = [
+  'Dependability',
+  'Interviewing',
+  'Mentoring',
+  'PM skills',
+  'Problem-solving',
+  'Teamwork',
+  'Wilingness to learn'
+]
