4af711a15ebbfe118f1d3f0c4d073a7aed5a9322
Vladyslav Ohirenko <aguretsvlad@gmail.com>
2022-04-17 14:49:32 +0300

    feat: update CV data

--- constants.mjs
+++ constants.mjs
@@ -1,24 +1,41 @@
-export const TECH_SKILLS = [
+const IM_GOOD_AT_TAGS = [
   'Docker',
   'JavaScript',
   'MongoDB',
   'NodeJS',
   'ReactJS',
-  'Unix'
-]
-
-export const HARD_SKILLS = [
+  'Unix',
+  'MySQL',
+  'PostgreSQL',
+  'Docker',
   'Data migration',
   'System migration',
   'Software designing',
-  'Web development'
+  'Web development',
+  'Interviewing',
+  'Problem-solving',
+  'GraphQL',
+  'TypeScript',
+  'Standardization',
+  'Communication',
 ]
 
-export const SOFT_SKILLS = [
-  'Dependability',
-  'Interviewing',
-  'Mentoring',
-  'PM skills',
-  'Problem-solving',
-  'Teamwork'
+export const RANDOMIZED_IM_GOOD_AT_TAGS = IM_GOOD_AT_TAGS
+  .map(value => ({ value, sort: Math.random() }))
+  .sort((a, b) => a.sort - b.sort)
+  .map(({ value }) => value)
+
+export const EDUCATION = [
+  {
+    title: 'Master Computer Sciences in Kharkiv Aerospace University',
+    years: '2016-2017'
+  },
+  {
+    title: 'Bachelor Computer Sciences in Kharkiv Aerospace University',
+    years: '2012-2016'
+  },
+  {
+    title: 'Kharkiv Aerospace Lyceum',
+    years: '2009-2012'
+  }
 ]
